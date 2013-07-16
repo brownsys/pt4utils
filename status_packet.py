6b46ea817a3d65573923ca3fceb2a01d6c919201
Marcelo Martins <martins@cs.brown.edu>
2013-07-16 16:24:01 -0400

    Add license header

--- status_packet.py
+++ status_packet.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#
+# This file is part of pt4utils.
+#
+# Copyright (C) 2013, Marcelo Martins.
+#
+# pt4utils was developed in affiliation with Brown University,
+# Department of Computer Science. For more information about the
+# department, see <http://www.cs.brown.edu/>.
+#
+# pt4utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pt4utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pt4utils.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 class StatusPacket(object):
 
     def __init__(self, length, packetType, firmwareVersion, protocolVersion,
